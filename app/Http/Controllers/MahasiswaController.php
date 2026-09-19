99122c5aabeb86ce60c69c60d4ca31e54708d167
farrelaliby <farrelaliby067@gmail.com>
2026-09-19 17:49:03 +0700

    commit pertemuan3

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    //
+    public function index()
+    {
+        $mahasiswa = [
+            'nim' => '251011700291',
+            'nama' => 'Muhammad Farrel Aliby',
+            'prodi' => 'Sistem Informasi',
+            'kampus' => 'Universitas Pamulang',
+            'email' => 'farrelaliby067@gmail.com',
+            'status' => 'aktif',
+         
+        ];
+
+        return view('mahasiswa', compact('mahasiswa'));
+    }
+}
